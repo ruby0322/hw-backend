2b41eb1338b8626e551939e04fffc45a373a8e04
Ruby <ruby0322@ntu.im>
2022-08-24 21:29:21 +0800

    .

--- src/routes/handler.ts
+++ src/routes/handler.ts
@@ -5,6 +5,12 @@ import 'dotenv/config';
 
 const router = express.Router();
 
+router.get('/', async (req: Request, res: Response) => {
+  res.send({
+    'message': 'Hello World!'
+  });
+});
+
 router.get('/info', async (req: Request, res: Response) => {
   const users = await User.find({});
   // console.log(users);
